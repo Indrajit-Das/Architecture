430a84aa133fbf4d4ffd6bf1105567b2988a226f
Indrajit-Das <indrojit210210@gmail.com>
2022-03-31 23:31:45 +0600

    Initial release only project set up

---
+++ resources/views/archee.blade.php
@@ -0,0 +1,387 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Archee</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <!--.............1.............-->
+    <div class="nav-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9">
+                    <nav class="navbar navbar-expand-md">
+                        <a class="navbar-brand" href="#">
+                            <div class="logo">
+                                <span><i class="fa fa-envelope-open" aria-hidden="true"></i></span>
+                                <span class="logo-logo ml-2">Archee</span>
+                            </div>
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+                            <ul class="navbar-nav ml-5">
+                                <li class="nav-item active ml-5">
+                                    <a class="nav-link " href="#">HOME</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">PROJECT</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">SERVICES</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">ABOUT</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <div class="col-sm-3">
+                    <div class="d-flex mt-2 justify-content-end">
+                        <div class="dropdown mr-3">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                Eng <i class="fa fa-angle-down" aria-hidden="true"></i>
+                            </a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Bangla</a>
+                                <a class="dropdown-item" href="#">English</a>
+                            </div>
+                        </div>
+                        <div class="link">
+                            <a href="#" class="nav-link">Contact</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-5 mt-5">
+                    <div class="nav-content mt-5">
+                        <h1>Archtechture that recogniseds all your sensibilities</h1>
+                        <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero modi natus dolor molestias rem voluptatem laborum mollitia, placeat distinctio in.</p>
+                        <div class="content-link">
+                            <a href="">Make An Enquiry</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-7 mt-5">
+                    <div class="nav-img">
+                        <img src="assets/img/Desktop%20-%2016.jpg" class="w-100">
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="rating">
+                        <span class="r-text">Ratings</span>
+                        <span class="ml-2"><i class="fa fa-star" aria-hidden="true"></i></span>
+                        <span class="ml-2">5.0</span>
+                        <h4 class="mt-2">Trusted by Clients From Worldwide</h4>
+                    </div>
+                    <div class="rating-msg mt-2">
+                        <img src="assets/img/11236aa7512eec2ef55c96f45031d4ba--man-fashion-casual-male-fashion.jpg">
+                        <img src="assets/img/11236aa7512eec2ef55c96f45031d4ba--man-fashion-casual-male-fashion.jpg">
+                        <img src="assets/img/11236aa7512eec2ef55c96f45031d4ba--man-fashion-casual-male-fashion.jpg">
+                        <span>+4k</span>
+                    </div>
+                </div>
+                <div class="col-sm-6 mt-3">
+                    <div class="mobile ml-5">
+                        <a href="#" class="text-decoration-none">++222343567756757</a>
+                        <p class="mt-2">Consectetur adipisicing elit. Esse facilis repellat doloribus laborum rem sapiente.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!--............./1.............-->
+
+    <!--.............2.............-->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="down-nav shadow-lg px-5 py-5">
+                    <h3>Search the price you looking for</h3>
+                    <div class="form mt-3">
+                        <form class="form-inline" action="/action_page.php">
+                            <div class="input">
+                                <input type="email" class="form-control" placeholder="Location" id="email">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                            </div>
+                            <div class="input">
+                                <input type="email" class="form-control" placeholder="Type" id="email">
+                                <i class="fa fa-home" aria-hidden="true"></i>
+                            </div>
+                            <div class="input">
+                                <input type="email" class="form-control" placeholder="Budget" id="email">
+                                <i class="fa fa-usd" aria-hidden="true"></i>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Search now</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--............./2.............-->
+
+    <!--.........................3..............................-->
+    <div class="container mt-5">
+        <div class="row ">
+            <div class="col-sm-4 mt-4">
+                <div class="section-two">
+                    <div class="icon">
+                        <i class="fa fa-hospital-o" aria-hidden="true"></i>
+                    </div>
+                    <h2 class="mt-3">Archee & Interior</h2>
+                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                    <a href="#" class="activee">120 project</a>
+                </div>
+            </div>
+            <div class="col-sm-4 mt-4">
+                <div class="section-two">
+                    <div class="icon">
+                        <i class="fa fa-hospital-o" aria-hidden="true"></i>
+                    </div>
+                    <h2 class="mt-3">Archee & Interior</h2>
+                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                    <a href="#">120 project</a>
+                </div>
+            </div>
+            <div class="col-sm-4 mt-4">
+                <div class="section-two">
+                    <div class="icon">
+                        <i class="fa fa-hospital-o" aria-hidden="true"></i>
+                    </div>
+                    <h2 class="mt-3">Archee & Interior</h2>
+                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                    <a href="#">120 project</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--........................./3..............................-->
+
+    <!--.........................4..............................-->
+    <div class="section pb-5 pt-2 mt-5">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-sm-7">
+                    <div class="about">
+                        <img src="assets/img/Desktop%20-%2016%20(1).jpg" class="w-100">
+                    </div>
+                </div>
+                <div class="col-sm-5 mt-4">
+                    <div class="about">
+                        <h1>We aim to set the trends of your modern living</h1>
+                        <p class="mt-3"> Praesentium voluptas asperiores minima officia earum accusantium voluptatibus deleniti officiis et, dignissimos animi repudiandae placeat, laboriosam accusantium vero consectetur laudantium laborum quaerat dolores asperiores quam at vel provident fugit, atque. laboriosam accusantium vero con</p>
+                        <div class="about-link mt-4">
+                            <a href="">Make An Enquiry</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--........................./4..............................-->
+
+    <!--.........................5..............................-->
+    <div class="container mt-5">
+        <h1>Selected Project</h1>
+        <div class="row">
+            <div class="col-sm-5">
+                <div class="project pr-one">
+                    <div class="bordered mt-2"></div>
+                    <h2 class="mt-3">Outdoor Structure</h2>
+                    <p> Delectus eos nemo sint quis consequuntur aliquam temporibus odit suscipit, quae harum, accusantium voluptatum dolores .</p>
+                </div>
+            </div>
+            <div class="col-sm-7">
+                <div class="project">
+                    <img src="assets/img/Desktop%20-%2016%20(3).jpg" class="w-100">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--........................./5..............................-->
+
+    <!--.........................7..............................-->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-7">
+                <div class="project">
+                    <img src="assets/img/Desktop%20-%2016%20(4).jpg" class="w-100">
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <div class="project pr-one">
+                    <div class="bordered mt-2"></div>
+                    <h2 class="mt-3">Outdoor Structure</h2>
+                    <p> Delectus eos nemo sint quis consequuntur aliquam temporibus odit suscipit, quae harum, accusantium voluptatum dolores .</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--........................./7..............................-->
+
+    <!--.........................8..............................-->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-5">
+                <div class="project pr-one">
+                    <div class="bordered mt-2"></div>
+                    <h2 class="mt-3">Outdoor Structure</h2>
+                    <p> Delectus eos nemo sint quis consequuntur aliquam temporibus odit suscipit, quae harum, accusantium voluptatum dolores .</p>
+                </div>
+            </div>
+            <div class="col-sm-7">
+                <div class="project">
+                    <img src="assets/img/Desktop%20-%2016%20(6).jpg" class="w-100">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--........................./5..............................-->
+    <!--.........................6..............................-->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-7">
+                <div class="project">
+                    <img src="assets/img/Desktop%20-%2016%20(7).jpg" class="w-100">
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <div class="project pr-one">
+                    <div class="bordered mt-2"></div>
+                    <h2 class="mt-3">Outdoor Structure</h2>
+                    <p> Delectus eos nemo sint quis consequuntur aliquam temporibus odit suscipit, quae harum, accusantium voluptatum dolores .</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--........................./6..............................-->
+    <!--.........................7..............................-->
+    <section class="arcitict pt-5 pb-5 mt-5">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="arc-content">
+                        <h1>Architects of Archee</h1>
+                    </div>
+                    <div class="arc-icon ml-4">
+                        <span><i class="fa fa-long-arrow-left" aria-hidden="true"></i></span>
+                        <span class="arc-active"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="arc-img">
+                        <img src="assets/img/11236aa7512eec2ef55c96f45031d4ba--man-fashion-casual-male-fashion.jpg" class="w-100">
+                        <div class="caption">
+                            <h3>Ralph Edwards</h3>
+                            <h4>Architect</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="arc-img">
+                        <img src="assets/img/11236aa7512eec2ef55c96f45031d4ba--man-fashion-casual-male-fashion.jpg" class="w-100">
+                        <div class="caption">
+                            <h3>Cody Fisher</h3>
+                            <h4>Interior Designer</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--........................./7..............................-->
+    <!--.........................8..............................-->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-6 mt-3">
+                <div class="content">
+                    <div class="img">
+                        <img src="assets/img/Desktop%20-%2016%20(2).jpg" class="w-100">
+                    </div>
+                    <div class="main-content mt-3">
+                        <span>16th Dec, 2021</span>
+                        <span class="ml-3 span"><i class="fa fa-circle" aria-hidden="true"></i> Easter Howard</span>
+                        <div class="bordered mt-2"></div>
+                        <h1 class="mt-3">How To Choose</h1>
+                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere repudiandae dolore aliquam vel saepe illum, exercitationem iure ad eius expedita! Facere repudiandae dolore aliquam vel saepe illum, exercitationem iure ad eius expedita!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 mt-3">
+                <div class="content">
+                    <div class="img">
+                        <img src="assets/img/Desktop%20-%2016%20(5).jpg" class="w-100">
+                    </div>
+                    <div class="main-content mt-3">
+                        <span>16th Dec, 2021</span>
+                        <span class="ml-3 span"><i class="fa fa-circle" aria-hidden="true"></i> Easter Howard</span>
+                        <div class="bordered mt-2"></div>
+                        <h1 class="mt-3">How To Choose</h1>
+                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere repudiandae dolore aliquam vel saepe illum, exercitationem iure ad eius expedita! Facere repudiandae dolore aliquam vel saepe illum, exercitationem iure ad eius expedita!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--........................./8..............................-->
+
+    <!--.........................9..............................-->
+    <footer>
+        <div class="container mt-5">
+            <div class="top-border mt-4"></div>
+            <div class="row mt-4">
+                <div class="col-sm-3">
+                    <div class="footer">
+                        <span><i class="fa fa-envelope-open" aria-hidden="true"></i></span>
+                        <span class="footer-logo ml-2">Archee</span>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="footer">
+                        <ul>
+                            <li>
+                                <a href="#">Zurich, Switzerland</a>
+                                <a href="#">Bahnhofstrasse 7/1</a>
+                                <a href="#">44 1793 123 456</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="footer ml-sm-5 text-center">
+                        <ul>
+                            <li>
+                                <a href="#">Project</a>
+                                <a href="#">Service</a>
+                                <a href="#">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="footer float-right">
+                        <ul>
+                            <li>
+                                <a href="#">Facebook</a>
+                                <a href="#">Instagram</a>
+                                <a href="#">Twitter</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--........................./9..............................-->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body></html>
